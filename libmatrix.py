d69b86edb4cd171a1e97d713d43b796da3f209cc
Mikhail Volkov <55005467+readiv@users.noreply.github.com>
2021-09-07 19:46:32 +0200

    pep8

--- libmatrix.py
+++ libmatrix.py
@@ -4,7 +4,8 @@ import logger
 
 log = logger.get_logger(__name__)
 
-dx_dy = [[0,1],[1,0],[0,-1],[-1,0]] # 4 направления обхода матрицы
+dx_dy = [[0, 1], [1, 0], [0, -1], [-1, 0]]  # 4 направления обхода матрицы
+
 
 class ExceptionMatrixNotCode200(Exception):
     """Класс исключения: ответ сервера не 200"""
@@ -12,35 +13,35 @@ class ExceptionMatrixNotCode200(Exception):
 
 
 def matrix2list_recursive(matrix_int, level):
-    if level == 0: #Конечные условия выхода при четном размере матрицы. 
+    if level == 0:  # Конечные условия выхода при четном размере матрицы.
         return []
-    x = y = (len(matrix_int) - level) // 2 #Начальные координаты
+    x = y = (len(matrix_int) - level) // 2  # Начальные координаты
     interim_list = [matrix_int[y][x]]
-    if level == 1: #Конечные условия выхода при нечетном размере матрицы. 
+    if level == 1:  # Конечные условия выхода при нечетном размере матрицы.
         return interim_list
-    #Начинаем обход матрицы
-    for n in range(4): #Четыре стороны
-        for i in range(level - 1): #число шагов
-            if ( n == 3 ) and (i == level - 2): #Последний обход на один шаг меньше
-                break 
+    # Начинаем обход матрицы
+    for n in range(4):  # Четыре стороны
+        for i in range(level - 1):  # число шагов
+            if (n == 3) and (i == level - 2):  # Последний обход на один шаг меньше
+                break
             x = x + dx_dy[n][0]
             y = y + dx_dy[n][1]
             interim_list.append(matrix_int[y][x])
-    return interim_list + matrix2list_recursive(matrix_int, level-2)
-    
+    return interim_list + matrix2list_recursive(matrix_int, level - 2)
 
-def matrix_str2int(matrix_text:str):
+
+def matrix_str2int(matrix_text: str):
     """ На входе строка с матрицей. На выходе двумерный массив с int """
     matrix_text = matrix_text.split("\n")
     matrix_int = []
 
     for line in matrix_text:
-        if len(line) == 0 or line[0] != "|": # Пропускаем все пустые и все не начинающиеся с |
+        if len(line) == 0 or line[0] != "|":  # Пропускаем все пустые и все не начинающиеся с |
             continue
         matrix_line = line[1:-1].split("|")
-        matrix_int.append([int(item.strip()) for item in matrix_line]) # Преобразование строк к int и добавляем к результату 
+        matrix_int.append([int(item.strip()) for item in matrix_line])  # Преобразование строк к int и добавляем к результату
     return matrix2list_recursive(matrix_int, len(matrix_int))
-    
+
 
 async def get_matrix(url: str) -> list[int]:
     """ На входе url к текстовому файлу с матрицей вида:
@@ -49,7 +50,7 @@ async def get_matrix(url: str) -> list[int]:
         +-----+-----+
         |   2 |  3  |
         +-----+-----+
-        Функция возвращает список, содержащий результат обхода полученной матрицы по спирали: 
+        Функция возвращает список, содержащий результат обхода полученной матрицы по спирали:
         против часовой стрелки, начиная с левого верхнего угла.
         При какой либо ошибке возвращает пустой список и пишет сообщение в лог
     """
@@ -58,7 +59,7 @@ async def get_matrix(url: str) -> list[int]:
             async with session.get(url, timeout=5) as response:
                 if response.status != 200:
                     raise ExceptionMatrixNotCode200(f"error: response status not ok. url:{url} status:{response.status}")
-                return matrix_str2int( await response.text() )
+                return matrix_str2int(await response.text())
     except Exception as e:
         log.error(e)
         return []
@@ -71,9 +72,7 @@ if __name__ == '__main__':
                   get_matrix("https://raw.githubusercontent.com/readiv/ls1/master/matrix3.txt"),
                   get_matrix("https://raw.githubusercontent.com/readiv/ls1/master/matrix4.txt"),
                   get_matrix("https://raw.githubusercontent.com/readiv/ls1/master/matrix5.txt"),
-                  get_matrix("https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt"),
-                  get_matrix("https://raw.githubusercontent.com/readiv/ls1/master/matrix32.txt")]
-
+                  get_matrix("https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt")]
 
     list_matrix = loop.run_until_complete(asyncio.gather(*coroutines))
     for matrix in list_matrix:
--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
-import logging, time
+import logging
 
-_log_format = f"%(asctime)s - [%(levelname)s] - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+_log_format = "%(asctime)s - [%(levelname)s] - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+
 
 def get_file_handler(file_name):
     file_handler = logging.FileHandler(file_name)
@@ -8,16 +9,17 @@ def get_file_handler(file_name):
     file_handler.setFormatter(logging.Formatter(_log_format))
     return file_handler
 
+
 def get_stream_handler():
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(logging.Formatter(_log_format))
     return stream_handler
 
-def get_logger(name, file_name:str = "libmatrix.log"):
+
+def get_logger(name, file_name: str = "libmatrix.log"):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     # logger.addHandler(get_file_handler(file_name))
     logger.addHandler(get_stream_handler())
     return logger
- 
